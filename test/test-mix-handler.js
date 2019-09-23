95503a738f91d9eee7c4168cadbd809d78ae6a03
aventador-lab <lamborcai@gmail.com>
2019-09-23 14:57:26 +0800

    auto commit

--- test/test-mix-handler.js
+++ test/test-mix-handler.js
@@ -1,3 +1,15 @@
+/**
+ * MetaMask Support
+ *  |\_/|,,_____,~~`
+ *  (.".)~~     )`~}}
+ *   \o/\ /---~\\ ~}}
+ *     _//    _// ~}
+ * 
+ * Copyright (c) 2019 NPL,aventador-lab
+ * E-mail :lamborcai@gmail.com
+ * git@work:minipaylab/token-demo.git
+ *
+ */
 const MixHandler = require('../src/packages/mix-handler.js');
 const TruffleConfig = require('../truffle-config.js');
 
